f000db03a223c1fa6feaa3ae548522b1288a3377
Adrianna Gerek <aduzja@gmail.com>
2023-07-15 18:46:03 +0200

    Create home page

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:what_to_do/app/base/utils/todo_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // list of todo
+  List toDoList = [
+    ['Work', 'Create a new app', false],
+    ['Shopping', 'Buy some food', false]
+  ];
+
+  // chceckbox onChanged
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][2] = !toDoList[index][2];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('What to do?'),
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ToDoList(
+            taskTitle: toDoList[index][0],
+            taskDescription: toDoList[index][1],
+            taskCompleted: toDoList[index][2],
+            onChanged: (value) => checkboxChanged(value, index),
+          );
+        },
+        itemCount: toDoList.length,
+      ),
+    );
   }
 }
