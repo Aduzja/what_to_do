f6f8972ca27e856a6521d56cecee75451396728c
Adrianna Gerek <aduzja@gmail.com>
2023-07-15 17:35:15 +0200

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:what_to_do/app/features/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'What to do?',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:what_to_do/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
