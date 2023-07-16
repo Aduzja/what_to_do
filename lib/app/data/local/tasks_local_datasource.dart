f998fc285a7b16bd82e470320c52ef16972864d5
Adrianna Gerek <aduzja@gmail.com>
2023-07-16 20:18:55 +0200

    Fix the problem with empty list

--- lib/app/data/local/tasks_local_datasource.dart
+++ lib/app/data/local/tasks_local_datasource.dart
@@ -3,15 +3,19 @@ import 'package:hive_flutter/hive_flutter.dart';
 class TasksLocalDatasource {
   List toDoList = [];
 
-  final _todoBox = Hive.box('todoBox');
+  final _toDoBox = Hive.box('toDoBox');
 
   // load data
   void loadData() {
-    toDoList = _todoBox.get('TODO list');
+    if (_toDoBox.containsKey('toDo list')) {
+      toDoList = _toDoBox.get('toDo list');
+    } else {
+      toDoList = [];
+    }
   }
 
   // update data
   void updateData() {
-    _todoBox.put('TODO list', toDoList);
+    _toDoBox.put('toDo list', toDoList);
   }
 }
